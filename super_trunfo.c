1b30148d38a160285b2c7555918e8c8d5a9613a4
Thiago Brazão <106168449+ThiagoBrazao@users.noreply.github.com>
2025-03-23 21:01:05 +0000

    Realizado o desafio nv_aventureiro 23-03-2025

--- super_trunfo.c
+++ super_trunfo.c
@@ -71,13 +71,30 @@ int main() {
 
     printf("Digite o número de pontos Turísticos: ");
     scanf("%d", &ponto_turistico2);
+
+    // Carta 1
+    // Realiza o calculo da divisão por habitantes em mêtros quadrados, na variável desidade_populacional_carta1.
+    // Faz a operação de divisão do pib com a população e armazena o resultado da riquesa média por pessoa na cidade na variável resultado_pibC1.
+    // Realizado a conversão implicita das variável populacao1 que é do tipo int para float.
+    float desidade_populacional_carta1 = populacao1 / area1;
+    float resultado_pibC1 = pib1 / populacao1;
     
-    printf("\n\n");  // Quebro duas linhas para mostrar os resultados formatados de acordo com a solicitação do desafio. 
+    // Carta 2 
+    // Realiza o calculo da divisão por habitantes em mêtros quadrados, na variável desidade_populacional_carta2.
+    // Faz a operação de divisão do pib com a população e armazena o resultado da riquesa média por pessoa na cidade na variável resultado_pibC2.
+    // Realizado a conversão implicita das variável populacao2 que é do tipo int para float.
+    float desidade_populacional_carta2 = populacao2 / area2;
+    float resultado_pibC2 = pib2 / populacao2;
+    
+
+    // Quebro duas linhas para mostrar os resultados formatados de acordo com a solicitação do desafio.
+    printf("\n\n");   
     
     // Resultado da primeira Carta.
     printf("Carta: %c\nEstado: %c\nCódigo: %s\n", carta1, estado1, codigo1);
     printf("Nome da Cidade: %s\nPopulação: %d\nÁrea: %.2f km²\n", cidade1, populacao1, area1);
     printf("PIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\n", pib1, ponto_turistico1);
+    printf("Densidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f reais\n", desidade_populacional_carta1, resultado_pibC1);
 
     printf("\n"); //Quebra de linha.
     
@@ -85,6 +102,10 @@ int main() {
     printf("Carta: %c\nEstado: %c\nCódigo: %s\n", carta2, estado2, codigo2);
     printf("Nome da Cidade: %s\nPopulação: %d\nÁrea: %.2f km²\n", cidade2, populacao2, area2);
     printf("PIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\n", pib2, ponto_turistico2);
+    
+    // Exibindo as informações das variáveis com valores com duas casa decimal. 
+    printf("Densidade Populacional: %.2f hab/km²\nPIB per Capita: %.2f reais\n", desidade_populacional_carta2, resultado_pibC2);
+    
     printf("\n");
     // Um mini banner de finalização do processo. 
     printf("===============================\n");
